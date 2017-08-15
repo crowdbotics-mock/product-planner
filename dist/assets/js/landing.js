9db839edcf947be6a895b34f749533fa21c71771
gabytrejo <gabytrejo303@gmail.com>
2017-08-15 16:50:33 +0000

    Firebase added

--- dist/assets/js/landing.js
+++ dist/assets/js/landing.js
@@ -1,3 +1,15 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyBOFC9R93z-5Yq6c4wNbT2OqK3nrAROunU",
+	authDomain: "product-planer.firebaseapp.com",
+	databaseURL: "https://product-planer.firebaseio.com",
+	projectId: "product-planer",
+	storageBucket: "product-planer.appspot.com",
+	messagingSenderId: "1039482368977"
+};
+firebase.initializeApp(config);
+
+
 
  $(document).ready(function(){
     $('.collapsible').collapsible();
