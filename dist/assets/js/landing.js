2d583cbc055e4ce6e563ef039ce14a9cc9b96075
gabytrejo <gabytrejo303@gmail.com>
2017-08-15 19:19:19 +0000

    functionality section2

--- dist/assets/js/landing.js
+++ dist/assets/js/landing.js
@@ -14,10 +14,12 @@ const cargarPagina = function(){
     $('.collapsible').collapsible();
     $('.modal').modal();
     $("#categoria").change(confirmacion);
+    $("#ue1").change(confirmacion2);
     $("#check1").click(seccion2);
-    $("#seccion1").submit(siguiente);
-    $("#seccion2").submit(siguiente);
-    $("#seccion3").submit(siguiente);
+    $("#check2").click(seccion3);
+    $("#seccion1").submit(siguiente1_2);
+    $("#seccion2").submit(siguiente2_3);
+    $("#seccion3").submit(siguiente3_4);
 
     
  };
@@ -28,16 +30,33 @@ const confirmacion = function(){
 	$("#correcto1").removeClass("hide");
 };
 
+const confirmacion2 = function(){
+	console.log("cambió");
+	$("#correcto2").removeClass("hide");
+};
+
 const seccion2 = function(){
 	console.log("sig de 1 a 2");
 	$("#correcto1s").removeClass("hide");
 };
 
-const siguiente = function(e){
+const seccion3 = function(){
+	console.log("sig de 2 a 3");
+	$("#correcto2s").removeClass("hide");
+};
+
+const siguiente1_2 = function(e){
 	e.preventDefault();
 	console.log("enter");
 	$("#inicio").addClass("hide");
 	$("#user-persona").removeClass("hide");
+};
+
+const siguiente2_3 = function(e){
+	e.preventDefault();
+	console.log("enter");
+	$("#user-persona").addClass("hide");
+	$("#acciones").removeClass("hide");
 };
  //Section1: Tipo de Prooducto
 
